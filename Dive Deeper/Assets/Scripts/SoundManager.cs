4f9d66e3ccd2e36cfb50a9804c6f33dd48f91653
Jordan Huynh <jordanhuynh784@gmail.com>
2023-08-22 14:41:58 -0700

    Sound Manager

--- Dive Deeper/Assets/Scripts/SoundManager.cs
+++ Dive Deeper/Assets/Scripts/SoundManager.cs
@@ -1,18 +1,87 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.Audio;
+using Yarn.Unity;
 
+/**
+Just add a sound and give it a name. Remember to set volume and make sure pitch is set to 1. Singleton, so you can call it wherever. 
+*/
 public class SoundManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [SerializeField] Sounds[] audioSounds;
+    public static SoundManager instance;
+
+    void Awake() {
+        if (instance == null) {
+            instance = this;
+        }
+
+        foreach (Sounds sounds in audioSounds) {
+            sounds.source = gameObject.AddComponent<AudioSource>();
+            sounds.source.playOnAwake = false;
+
+            sounds.source.clip = sounds.audioClip;
+
+            sounds.source.volume = sounds.soundVol;
+            sounds.source.pitch = sounds.soundPitch;
+            sounds.source.loop = sounds.isLooping;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    [YarnCommand("play_sound")]
+    public void PlaySound(string name, bool looping) {
+        Sounds sound = Array.Find(audioSounds, sounds => sounds.soundName == name);
+
+        if (sound == null) {
+            Debug.LogWarning("Audio: " + name + " missing");
+            return;
+        }
+
+        sound.source.Play();
+        sound.source.loop = looping;
+        Debug.Log("Playing " + name);
+    }
+
+    [YarnCommand("stop_sound")]
+    public void StopSound(string name) {
+        Sounds sound = Array.Find(audioSounds, sounds => sounds.soundName == name);
+
+        if (sound == null) {
+            Debug.LogWarning("Audio: " + name + " missing");
+            return;
+        }
+
+        sound.source.Stop();
+    }
+
+    [YarnCommand("pause_sound")]
+    public void PauseSound(string name) {
+        Sounds sound = Array.Find(audioSounds, sounds => sounds.soundName == name);
+
+        if (sound == null) {
+            Debug.LogWarning("Audio: " + name + " missing");
+            return;
+        }
+
+        sound.source.Pause();
+    }
+
+    [YarnCommand("unpause_sound")]
+    public void UnpauseSound(string name) {
+        Sounds sound = Array.Find(audioSounds, sounds => sounds.soundName == name);
+
+        if (sound == null) {
+            Debug.LogWarning("Audio: " + name + " missing");
+            return;
+        }
+
+        sound.source.UnPause();
+    }
+
+    public void AdjustVolume(float volume) {
+        foreach (Sounds sound in audioSounds) {
+            sound.soundVol = volume;
+            sound.source.volume = volume;
+        }
     }
 }
---
+++ Dive Deeper/Assets/Scripts/Sounds.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.Audio;
+
+[System.Serializable]
+public class Sounds
+{
+    public AudioClip audioClip;
+
+    public string soundName;
+
+    [Range(0f, 1f)]
+    public float soundVol;
+    [Range(-3f, 3f)]
+    public float soundPitch;
+
+    public bool isLooping;
+
+    [HideInInspector]
+    public AudioSource source;
+}
