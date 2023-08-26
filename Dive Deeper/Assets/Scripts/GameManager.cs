acc524cb9eed6a08157aaa4c1d222925211d514a
Jordan Huynh <jordanhuynh784@gmail.com>
2023-08-25 22:21:23 -0700

    Adding new scripts

--- Dive Deeper/Assets/Scripts/GameManager.cs
+++ Dive Deeper/Assets/Scripts/GameManager.cs
@@ -1,33 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using Yarn.Unity;
 
 public class GameManager : MonoBehaviour
 {
     private static GameManager _instance;
     public static GameManager Instance
+
     {
         get
         {
             if (_instance == null)
             {
                 _instance = FindObjectOfType<GameManager>();
+                
             }
             return _instance;
         }
     }
 
-    public InventoryItem selectedItem;
+    public int sceneIndex;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        DontDestroyOnLoad(gameObject);
+        UnityEngine.SceneManagement.SceneManager.sceneLoaded += UpdateScene;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    public void UpdateScene(Scene s, LoadSceneMode mode) {
+        sceneIndex = UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex;
+    }
 
+    [YarnCommand("load_last_scene")]
+    public void LoadLastScene() {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneIndex);
     }
 }
